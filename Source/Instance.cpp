2ee0069ee867af75d944418359a62a8b66a8e5c8
Madman10K <stanv.contact176@gmail.com>
2023-06-05 17:44:09 +0300

    Add new icon

--- Source/Instance.cpp
+++ Source/Instance.cpp
@@ -2,18 +2,146 @@
 
 ude_welcome::Instance::Instance()
 {
-    initInfo.inlineComponents.push_back(&welcome);
+    //initInfo.inlineComponents.push_back(&welcome);
 }
 
 void ude_welcome::Instance::begin()
 {
     beginAutohandle();
-
+    madladsquad.loadImGui();
+    //for (auto& a : softwareSuite)
+    //    a.icon.loadImGui();
+    //for (auto& a : deSoftware)
+    //    a.icon.loadImGui();
+    //for (auto& a : flowSoftware)
+    //    a.icon.loadImGui();
 }
 
 void ude_welcome::Instance::tick(float deltaTime)
 {
     tickAutohandle(deltaTime);
+    ImGui::ShowDemoWindow((void*)nullptr);
+    if (ImGui::BeginTabBar("Test"))
+    {
+        if (ImGui::BeginTabItem("Welcome", (bool*)nullptr))
+        {
+            ImGui::TextWrapped("Welcome to the Untitled Desktop Environment!\n\nThe completely independent and "
+                               "highly hackable desktop environment for Linux and other systems based on "
+                               "components by the freedesktop foundation.");
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("About us", (bool*)nullptr))
+        {
+            ImGui::TextWrapped("The UntitledDesktopEnvironment is developed by MadLadSquad, a Bulgarian free "
+                               "software collective. These are our priorities:");
+            ImGui::BulletText("User freedom - Replace any application, modify application features, etc");
+            ImGui::BulletText("Cross-desktop compatibility - Adapter programs to merge QT and GTK based desktops "
+                              "with our own");
+            ImGui::BulletText("A fully community-centered approach to development - Everyone is a user");
+            ImGui::BulletText("A great UX, no matter the user's experience - Because all users are valid");
+            ImGui::BulletText("A great multilingual experience - Your human language matters");
+            ImGui::BulletText("A great distribution experience - Have the latest version of applications "
+                              "available within a day");
+            ImGui::BulletText("A great theming experience - Full and easy customisation using cross-desktop "
+                              "theming and format adapters");
+
+            ImGui::Image((void*)(intptr_t)madladsquad.get(), { 50.0f, 50.0f });
+            if (ImGui::IsItemHovered())
+            {
+                ImGui::BeginTooltip();
+                ImGui::Text("MadLadSquad logo");
+                ImGui::EndTooltip();
+            }
+            ImGui::SameLine();
+            ImGui::Image((void*)(intptr_t)flow.get(), { 50.0f, 50.0f });
+
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Software", (bool*)nullptr))
+        {
+            int gridItems = static_cast<int>(ImGui::GetContentRegionAvail().x / 50);
+            size_t i = 0;
+            ImGui::Text("UntitledApplicationSuite");
+            if (ImGui::BeginTable("UntitledApplicationSuite table", gridItems))
+            {
+                ImGui::NextColumn();
+
+                for (auto& a : softwareSuite)
+                {
+                    ImGui::PushID(i);
+
+                    std::cout << i << std::endl;
+
+                    ImGui::Image((void*)(intptr_t)a.icon->get(), { 50.0f, 50.0f });
+                    if (ImGui::IsItemHovered())
+                    {
+                        ImGui::BeginTooltip();
+                        ImGui::Text("%s", a.about.c_str());
+                        ImGui::EndTooltip();
+                    }
+                    ImGui::TextWrapped("%s", a.name.c_str());
+                    ImGui::NextColumn();
+
+                    ImGui::PopID();
+                    i++;
+                }
+                ImGui::EndTable();
+            }
+            ImGui::Text("UntitledDesktop applications");
+            if (ImGui::BeginTable("UDE Application table", gridItems))
+            {
+                ImGui::NextColumn();
+                for (auto& a : softwareSuite)
+                {
+                    ImGui::PushID(i);
+
+                    ImGui::Image((void*)(intptr_t)a.icon->get(), { 50.0f, 50.0f });
+                    if (ImGui::IsItemHovered())
+                    {
+                        ImGui::BeginTooltip();
+                        ImGui::Text("%s", a.about.c_str());
+                        ImGui::EndTooltip();
+                    }
+                    ImGui::TextWrapped("%s", a.name.c_str());
+                    ImGui::NextColumn();
+
+                    ImGui::PopID();
+                    i++;
+                }
+                ImGui::EndTable();
+            }
+
+            ImGui::Text("UntitledDesktopFlow");
+            if (ImGui::BeginTable("UntitledDesktop Flow applications", gridItems))
+            {
+                ImGui::NextColumn();
+                for (auto& a : softwareSuite)
+                {
+                    ImGui::PushID(i);
+
+                    ImGui::Image((void*)(intptr_t)a.icon->get(), { 50.0f, 50.0f });
+                    if (ImGui::IsItemHovered())
+                    {
+                        ImGui::BeginTooltip();
+                        ImGui::Text("%s", a.about.c_str());
+                        ImGui::EndTooltip();
+                    }
+                    ImGui::TextWrapped("%s", a.name.c_str());
+                    ImGui::NextColumn();
+
+                    ImGui::PopID();
+                    i++;
+                }
+                ImGui::EndTable();
+            }
+            ImGui::EndTabItem();
+        }
+        if (ImGui::BeginTabItem("Plugins", (bool*)nullptr))
+        {
+            ImGui::EndTabItem();
+        }
+        ImGui::EndTabBar();
+    }
 
 }
 
@@ -32,9 +160,3 @@ void ude_welcome::Instance::onEventConfigureStyle(ImGuiStyle& style, ImGuiIO& io
 {
 
 }
-
-void ude_welcome::Instance::next() noexcept
-{
-    page++;
-}
-
--- Source/Instance.hpp
+++ Source/Instance.hpp
@@ -1,9 +1,15 @@
 #pragma once
 #include <Framework.hpp>
-#include <Welcome.hpp>
 
 namespace ude_welcome
 {
+    struct Application
+    {
+        UImGui::Texture* icon;
+        UImGui::FString name;
+        UImGui::FString about;
+    };
+
     class UIMGUI_PUBLIC_API Instance : public UImGui::Instance
     {
     public:
@@ -14,15 +20,122 @@ namespace ude_welcome
         virtual ~Instance() override;
 
         virtual void onEventConfigureStyle(ImGuiStyle& style, ImGuiIO& io) override;
-
-        void next() noexcept;
     private:
-        Welcome welcome;
+        UImGui::Texture madladsquad = UImGui::Texture("../Content/madladsquadlogo.png");
+        UImGui::Texture flow;
 
-        UImGui::InlineComponent* components = { &welcome };
+        std::vector<Application> softwareSuite =
+        {
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledTextEditor",
+                .about = "A cross-platform, minimal, yet highly customisable text editor"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledImageViewer",
+                .about = "A cross-platform, minimal, yet highly customisable image viewer"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledCalculator",
+                .about = "A cross-platform, minimal, yet highly customisable calculator"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledLinuxGameManager",
+                .about = "A game system linux container manager application"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledIBusHandwrite",
+                .about = "A handwriting input method for IBus"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledTerminalEmulator",
+                .about = "A cross-platform, minimal, yet highly customisable terminal emulator with graphical "
+                         "acceleration"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledFileManager",
+                .about = "A cross-platform, minimal, yet highly customisable file manager"
+            },
+        };
 
+        std::vector<Application> deSoftware =
+        {
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDESessionLogout",
+                .about = "A session logout prompt"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDEWelcome",
+                .about = "An application for welcoming new user and for storing documentation.\n"
+                         "The application you're currently using"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDEPolkitAgent",
+                .about = "A polkit agent for the desktop environment"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledKBD",
+                .about = "A keyboard layout conversion application"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDesktopCompat",
+                .about = "A cross-desktop multi-way compatibility adapter system"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDEPluginPackageManager",
+                .about = "A package manager for plugins for the UntitledDesktopEnvironment"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDEThemeGenerator",
+                .about = "A generator for UDE themes and helper theme maker application"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDESessionManager",
+                .about = "A session manager for the UntitledDesktopEnvironment"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDEPowerManager",
+                .about = "A power manager for the UntitledDesktopEnvironment"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDEWallpaperManager",
+                .about = "A wallpaper manager for the UntitledDesktopEnvironment"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UntitledDESettings",
+                .about = "A settings and configuration manager for the UntitledDesktopEnvironment"
+            },
+        };
 
-        size_t page = 0;
+        std::vector<Application> flowSoftware = {
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UDFApplicationLauncher",
+                .about = "An application launcher for the UntitledDesktop Flow"
+            },
+            {
+                .icon = &madladsquad,//("../Content/madladsquadlogo.png"),
+                .name = "UDFPanel",
+                .about = "A panel for the UntitledDesktop Flow"
+            },
+        };
     };
 }
 
